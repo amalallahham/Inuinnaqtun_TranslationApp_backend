8c1e57461f675dc3c69c7190d9671eba27d3fad0
amalallaham <amalallahham20@gmail.com>
2025-03-11 14:22:07 -0700

    dev branch

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
     console.log("MongoDB Connected");
+    
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
