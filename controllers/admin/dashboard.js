348b42c9f566e37d88dbcda3015b8e862aa1d792
Soomin Jeong <sjeong0643@gmail.com>
2025-03-31 20:41:02 -0700

    Fixing routing errors

--- controllers/admin/dashboard.js
+++ controllers/admin/dashboard.js
@@ -1,6 +1,10 @@
 import User  from "../../models/users.js";
 
 export const get_dashboard = async (req, res) => {
-  res.render("index", { title: "Dashboard", user: req.session });
+  // res.render("index", { title: "Dashboard", user: req.session });
+  if (!req.session || !req.session.adminId) {
+    res.render("translate", { error: "", title: "Translate", isAdmin: false });
+  }
+  res.render("translate", { error: "", title: "Translate", isAdmin: true });
 };
 
