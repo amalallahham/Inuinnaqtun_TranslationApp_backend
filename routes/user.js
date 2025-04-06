b85791defb42e62072bab4bc3042c784c74a6750
Eugenie Kim <eugenie.kim@outlook.com>
2025-04-06 12:12:13 -0700

    Added ability to see similar words and audio playing option on words present in the database when translated

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUsers } from "../controllers/users.js";
-import { get_translate, translate_text } from "../controllers/translation/translate.js";
+import { get_translate, translate_text, getWordDetails } from "../controllers/translation/translate.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.get("/", getUsers);
 
 router.get("/translate", get_translate);
 
+router.get("/word-details/:id", getWordDetails);
+
 router.post("/translate", translate_text);
+
 export default router;
